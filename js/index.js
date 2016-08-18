a9545aa505124e5436cd1d57e7675685386beb43
Arthelon <hsing.daniel@gmail.com>
2016-08-18 13:33:29 +0800

    Changed navbar color theme to white, added waypoint handlers, added jQuery scroll for navbar

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
 (function() {
-	var sections = []
+	var scrollSpeed = 400
+
+	function clearActiveNavs() {
+		$("#nav-links li").each(function() {
+			$(this).attr("class", "")
+		})
+	}
+
 	$("section").each(function() {
-		sections.push($(this).waypoint({
+		var $this = $(this)
+		$this.waypoint({
 			handler: function() {
-				moveNavTo(this.element.id)
+				clearActiveNavs()
+				var id = $this.attr("id")
+				$("#nav-links li a[href='#" + id + "']").parent().attr("class", "active")
 			}
-		}))
+		})
 	})
 
-	function moveNavTo(id) {
-		
-	}
+	$("header").waypoint({
+		handler: function() {
+			clearActiveNavs()
+		}
+	})
 
-	function scrollToById(id) {
-		
-	}
+	//Navbar click handlers
+	$("#nav-logo > a").click(function(e) {
+		e.preventDefault()
+		$(window).scrollTo(0, scrollSpeed)
+	})
+
+	$("#nav-links > li a").click(function(e) {
+		e.preventDefault()
+		var target = $(this).attr("href")
+		$(window).scrollTo($(target), scrollSpeed)
+	})
 })()
